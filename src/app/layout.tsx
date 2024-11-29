00f087d8d2a1d44e2a6248f686c0a15d669c427a
simplesphere <anoop.jeewoolall@gmail.com>
2024-11-29 12:15:27 -0500

    chore: prettier + lint staged + husky + config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import '@/assets/globals.css';
-import { TChildren } from '@/types';
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import '@/assets/globals.css'
+import { TChildren } from '@/types'
 
 const satoshi = localFont({
 	src: '../assets/fonts/satoshi.woff2',
 	variable: '--font-satoshi',
-});
+})
 
 export const metadata: Metadata = {
 	title: 'Basic Next.js boilerplate',
 	description: 'A basic Next.js boilerplate with TypeScript, Tailwind CSS, and ESLint.',
-};
+}
 
 export default function RootLayout({ children }: Readonly<TChildren>) {
 	return (
 		<html lang="en">
 			<body className={`${satoshi.variable} font-sans text-slate-700`}>{children}</body>
 		</html>
-	);
+	)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,18 @@ export default function Home() {
 			<div className="container">
 				<h1 className="text-3xl mb-3 font-bold">NextJs Boilerplate (minimal)</h1>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non odio nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris hendrerit et quam at iaculis. In vel
-					diam vel nibh rutrum interdum eu nec tortor. Proin vel arcu viverra, vehicula orci nec, tincidunt justo. Mauris vel sapien et felis venenatis rhoncus id sed nisl.
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non odio nunc.
+					Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris hendrerit et quam at
+					iaculis. In vel diam vel nibh rutrum interdum eu nec tortor. Proin vel arcu viverra,
+					vehicula orci nec, tincidunt justo. Mauris vel sapien et felis venenatis rhoncus id sed
+					nisl.
 				</p>
 				<p>
-					Sed quis turpis pharetra nunc pellentesque interdum. Vivamus semper quis dolor vestibulum suscipit. Cras non ante at nulla gravida lacinia et sed leo. Donec cursus at turpis non malesuada.
+					Sed quis turpis pharetra nunc pellentesque interdum. Vivamus semper quis dolor vestibulum
+					suscipit. Cras non ante at nulla gravida lacinia et sed leo. Donec cursus at turpis non
+					malesuada.
 				</p>
 			</div>
 		</section>
-	);
+	)
 }
