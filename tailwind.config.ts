02944ec2f10ae4ee2f3e4c2c0d826fa3c57e82c5
simplesphere <anoop.jeewoolall@gmail.com>
2024-11-29 10:17:21 -0500

    refactor: cleanup - removed images / unused code + added font + updated stylesheet + added alias

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+	content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+		},
+		extend: {
+      fontFamily: {
+        sans: ['var(--font-satoshi)', 'ui-sans-serif', 'system-ui'],
       },
-    },
-  },
-  plugins: [],
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
